d754238d2d94307a67b97acec73c104f78ee8353
Svlsqz <100167884+Svlsqz@users.noreply.github.com>
2025-01-14 20:16:56 -0400

    Delete prueba.php

--- prueba.php
+++
@@ -1,7 +0,0 @@
-<?php
-require_once 'GestionarTareas.php';
-
-$gestor = new GestionarTareas();
-$gestor->añadirTarea("Estudiar PHP");
-$gestor->añadirTarea("Completar ejercicio de programación");
-?>
